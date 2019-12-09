a786b0657a4c47097556a5711b40dadc97d79759
Sameer Desai <48178078+sameerdesai123@users.noreply.github.com>
2019-12-09 18:49:45 +0530

    Added build folder

---
+++ build/precache-manifest.1dfdfce9163d0b0426089f4809163e8e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed77732a342d5849b578b9237931c69d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c59581f5654a3da5abff",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f3584fe33db9fcb699ae",
+    "url": "/static/css/main.abe1a40c.chunk.css"
+  },
+  {
+    "revision": "c59581f5654a3da5abff",
+    "url": "/static/js/2.8fa03091.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.8fa03091.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f3584fe33db9fcb699ae",
+    "url": "/static/js/main.8bfa1190.chunk.js"
+  },
+  {
+    "revision": "8e6b760baaab6a168bc5",
+    "url": "/static/js/runtime-main.3599eb91.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
